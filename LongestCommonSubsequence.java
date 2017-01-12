e9390448da2a3ea630754a4c359cdb659fb86070
Vaibhav Sahu <vaibh3jec@gmail.com>
2017-01-12 01:22:11 -0700

    Updated code to print the common subsequence

--- LongestCommonSubsequence.java
+++ LongestCommonSubsequence.java
@@ -9,6 +9,8 @@ package com.vaibhavsahu.interviews;
 * Expected length: 4(Sequence: GTAB)
 * */
 
+import com.sun.tools.doclets.formats.html.SourceToHTMLConverter;
+
 public class Main {
 
     public static int getLengthLCS(String s1, String s2, int m , int n){
@@ -29,14 +31,41 @@ public class Main {
             }
 
         }
+        System.out.println(printLCSSequence(s1, s2, lengthLCS));
 return lengthLCS[m][n];
 
     }
+
+    public static String printLCSSequence(String s1, String s2, int [][]LCS) {
+        StringBuffer stringBuffer = new StringBuffer();
+        int l1 = s1.length();
+        int l2 = s2.length();
+
+        for (int i = l1, j = l2; i != 0 && j != 0; ) {
+            if (LCS[i][j] == LCS[i - 1][j])
+                i--;
+            else if (LCS[i][j] == LCS[i][j - 1])
+                j--;
+            else {
+                assert s1.charAt(i - 1) == s2.charAt(j - 1);
+                stringBuffer.append(s1.charAt(i - 1));
+                i--;
+                j--;
+
+            }
+
+
+        }
+        return stringBuffer.reverse().toString();
+    }
+
 
     public static void main(String[] args) {
 	// write your code here
         String str1 = "AGGTAB";
         String str2 = "GXTXAYB";
         System.out.println(getLengthLCS(str1, str2, str1.length(), str2.length()));
+       // int [][] LCS = new int[str1.length()+1][str2.length()+1];
+        //System.out.println(printLCSSequence(str1, str2));
     }
 }
